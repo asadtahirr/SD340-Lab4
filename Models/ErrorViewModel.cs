2f1bb2593c3b6f7c2d2298eb0d39b5bfb4c41334
asad <asadtahir9999@gmail.com>
2022-09-17 00:50:55 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SD340_Lab4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
